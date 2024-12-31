bf15a2be52bd1cbb34c1d2bbeaf989ea3144ba2a
codingprodigy123 <sciencykidgenius123@gmail.com>
2025-01-01 00:10:48 +1300

    Add files via upload
    run ai.py in code editor or using the the first command in pi ai cmd.txt

---
+++ ai.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+#from langchain_ollama import OllamaLLM
+
+#model = OllamaLLM(model='mistral')
+window = tk.Tk()
+window.geometry('500x500')
+window.title('pi ai') 
+
+def make_responce():
+    x = input_field.get()
+    y = model.invoke(x)
+    output_field.insert(tk.END, f'ai: {y}\n')
+
+input_field = tk.Entry(window, width=30)
+input_field.pack(pady=20)
+
+generate_button = tk.Button(window, text='Generate', command=make_responce)
+generate_button.pack(pady=10)
+
+output_field = tk.Text(window, height=10, width=50)
+output_field.pack(pady=20)
+
+window.mainloop()
+while True:
+    pass
+#   x = input('me:')
+#   y = model.invoke(x)
+#   print(f'ai:{y}')
